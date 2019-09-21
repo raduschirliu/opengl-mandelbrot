a3eae5496456796492f0357a8c44a70a736b8961
Radu Schirliu <radu.schirliu@gmail.com>
2019-09-20 19:42:57 -0600

    Fixed issue with selection accuracy

--- OpenGL-Mandelbrot/main.cpp
+++ OpenGL-Mandelbrot/main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include "program.h"
 
+
 GLFWwindow* window;
 Program program;
 int iters = 300;
@@ -14,6 +15,8 @@ double mouseStartX, mouseStartY;
 double minX = -2, maxX = 1;
 double minY = -1.5, maxY = 1.5;
 
+
+// Linearly interpolates between two values
 double lerp(double start, double end, double p)
 {
 	return start + (end - start) * p;
@@ -24,18 +27,21 @@ double map(double value, double inMin, double inMax, double outMin, double outMa
 	return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
 }
 
+// Called on GLFW errors
 void errorCallback(int error, const char* desc)
 {
 	printf("\nGLFW Error: %s\n\n", desc);
 }
 
+// Called on mouse events
 void mouseCallback(GLFWwindow* window, int button, int action, int mods)
 {
+	// Reset view by right clicking
 	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
 	{
 		if (isDragging)
 		{
-			printf("Cancelled zoom\n");
+			printf("Cancelled selection\n");
 			isDragging = false;
 		}
 		else
@@ -50,32 +56,63 @@ void mouseCallback(GLFWwindow* window, int button, int action, int mods)
 		return;
 	}
 
-	if (button != GLFW_MOUSE_BUTTON_LEFT) return;
-
 	double x, y;
 	glfwGetCursorPos(window, &x, &y);
+
+	if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS)
+	{
+		double xp = map(x, 0, width, minX, maxX);
+		double yp = map(y, 0, height, minY, maxY);
+		printf("(%lf, %lf)\t(%lf, %lf)\n", xp, yp, x, y);
+
+		return;
+	}	
+
+	// Choose new view rect by left-click and dragging
+	if (button != GLFW_MOUSE_BUTTON_LEFT) return;
 
 	if (action == GLFW_PRESS)
 	{
 		isDragging = true;
+
+		// Contain values to window
+		if (x < 0) x = 0;
+		if (y < 0) y = 0;
+
+		if (x > width) x = width;
+		if (y > height) y = height;
+
 		mouseStartX = x;
 		mouseStartY = y;
-
-		printf("(%f, %f)\n", x, y);
 	}
 	else if (action == GLFW_RELEASE && isDragging)
 	{
+		// Prevent accidentally setting viewport to be 1x1 px
+		if (mouseStartY == y && mouseStartX == x)
+		{
+			isDragging = false;
+			return;
+		}
+
+		// Contain values
+		if (x < 0) x = 0;
+		if (y < 0) y = 0;
+
+		if (x > width) x = width;
+		if (y > height) y = height;
+
 		double newMinX = map(mouseStartX, 0, width, minX, maxX);
-		double newMinY = map(mouseStartY, 0, height, minY, maxY);
 		double newMaxX = map(x, 0, width, minX, maxX);
-		double newMaxY = map(y, 0, height, minY, maxY);
+
+		// Min and max flipped since 0 on Im axis is at the bottom, and 0 in OpenGL was mapped to the top
+		double newMinY = map(mouseStartY, 0, height, maxY, minY);
+		double newMaxY = map(y, 0, height, maxY, minY);
 
 		minX = std::min(newMinX, newMaxX);
 		maxX = std::max(newMinX, newMaxX);
 
-		// Negative vertical axis of a complex plane is down. Negative vertical axis of screen plane is up
-		minY = std::max(newMinY, newMaxY);
-		maxY = std::min(newMinY, newMaxY);
+		minY = std::min(newMinY, newMaxY);
+		maxY = std::max(newMinY, newMaxY);
 
 		isDragging = false;
 
@@ -83,6 +120,7 @@ void mouseCallback(GLFWwindow* window, int button, int action, int mods)
 	}
 }
 
+// Called on scroll events
 void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
 {
 	iters = iters + yOffset * 300;
@@ -92,6 +130,28 @@ void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
 	printf("Iterations: %i\n", iters);
 }
 
+// Apply shader and draw mandelbrot
+void drawMandelbrot(int x, int y, int w, int h)
+{
+	program.use();
+	program.setUniform("width", width);
+	program.setUniform("height", height);
+	program.setUniform("iters", iters);
+
+	program.setUniform("xBounds", minX, maxX);
+	program.setUniform("yBounds", minY, maxY);
+
+	glBegin(GL_QUADS);
+		glVertex2f(x, y);
+		glVertex2f(w, y);
+		glVertex2f(w, h);
+		glVertex2f(x, h);
+	glEnd();
+
+	program.clear();
+}
+
+// Called to update window drawing
 void draw()
 {
 	glfwGetFramebufferSize(window, &width, &height);
@@ -103,22 +163,7 @@ void draw()
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	program.use();
-	program.setUniform("width", width);
-	program.setUniform("height", height);
-	program.setUniform("iters", iters);
-
-	program.setUniform("xBounds", minX, maxX);
-	program.setUniform("yBounds", minY, maxY);
-
-	glBegin(GL_QUADS);
-		glVertex2f(0, 0);
-		glVertex2f(width, 0);
-		glVertex2f(width, height);
-		glVertex2f(0, height);
-	glEnd();
-
-	program.clear();
+	drawMandelbrot(0, 0, width, height);
 
 	if (isDragging)
 	{
@@ -138,6 +183,7 @@ void draw()
 	glfwPollEvents();
 }
 
+// Create window and initialize GLFW
 int main()
 {
 	printf("Running...\n\n");
